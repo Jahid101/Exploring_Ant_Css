677129d47192153076ea746a903e45794eb24677
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-14 01:23:16 +0600

    button

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Homepage from './Components/Homepage';
 import 'antd/dist/antd.css';
+import AntCss from './Components/AntCss/index'
 
 
 export default function Home() {
 
   return (
-    <div>
-      <Homepage />
+    <div className="">
+      <h1 className="">Exploring Ant Css</h1>
+      <AntCss/>
     </div>
   )
 }
