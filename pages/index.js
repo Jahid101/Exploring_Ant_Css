a99ae25ccc5170d24e529bcc92665b3f11025252
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-16 01:38:21 +0600

    autoComplete

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
   const [size, setSize] = useState(8);
 
-  // const router = useRouter()
+  const router = useRouter()
   // const { page } = router.query
 
   const handleClick = (component) => {
@@ -35,6 +35,8 @@ export default function Home() {
 
   return (
     <div className="ph-30 pv-30">
+      <h1 className="text-center underline">Exploring Ant Design</h1>
+
       <Layout className="layout">
         <h1 className="text-center">Session: 01</h1>
         <Header>
@@ -61,7 +63,7 @@ export default function Home() {
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['0']}>
             {new Array(15).fill(null).map((_, index) => {
               const key = index + 1;
-              const sense = ["", "autoComplete", "cascader", "checkbox", "datepicker", "form", "input" ,"inputNumber","mention","radio","rate","select", "slider","switch","timePicker","transfer","treeSelect","upload"]
+              const sense = ["", "autoComplete", "cascader", "checkbox", "datepicker", "form", "input", "inputNumber", "mention", "radio", "rate", "select", "slider", "switch", "timePicker", "transfer", "treeSelect", "upload"]
               return <Menu.Item
                 onClick={() => handleClick(sense[key])}
                 key={key}
@@ -71,12 +73,12 @@ export default function Home() {
             })}
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
+        {/* <Content style={{ padding: '0 50px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
-            {/* <Breadcrumb.Item>{page}</Breadcrumb.Item> */}
+            <Breadcrumb.Item>{page}</Breadcrumb.Item>
           </Breadcrumb>
           <div className="site-layout-content">Exploring Ant Css</div>
-        </Content>
+        </Content> */}
       </Layout>
     </div>
   )
