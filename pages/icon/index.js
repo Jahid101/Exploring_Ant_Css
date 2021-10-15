f74b95e9382e1db4743561277406f9bfc164d85e
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-15 23:53:43 +0600

    menu and dropdown added

--- pages/icon/index.js
+++ pages/icon/index.js
@@ -4,16 +4,43 @@ import React from 'react';
 const index = () => {
 
     return (
-        <div className="icons-list">
-            <HomeOutlined />
-            <SyncOutlined spin />
-            <SmileOutlined rotate={180} />
-            <LoadingOutlined />
+        <div className="icons-list mv-50 mh-50">
+            <h1 className="text-center underline">Hey there! its all about icons</h1>
 
-            <PlusCircleTwoTone />
-            <PlayCircleOutlined />
-            <TrophyTwoTone />
-            <PlayCircleFilled />
+            <HomeOutlined
+                style={{ color: 'black', backgroundColor: 'blue', fontSize: '50px', margin:'30px' }}
+            />
+
+            <SyncOutlined
+                style={{ color: 'blue', backgroundColor: 'red', fontSize: '50px', margin:'30px' }}
+                spin
+            />
+
+            <SmileOutlined
+                style={{ color: 'black', backgroundColor: 'yellow', fontSize: '50px', margin:'30px' }}
+                rotate={0}
+            />
+
+            <LoadingOutlined
+                style={{ color: 'blue', backgroundColor: 'white', fontSize: '50px', margin:'30px' }}
+            />
+
+            <PlusCircleTwoTone
+                style={{ color: 'red', backgroundColor: 'green', fontSize: '50px', margin:'30px' }}
+            />
+
+            <PlayCircleOutlined
+                style={{ color: 'purple', backgroundColor: 'orange', fontSize: '50px', margin:'30px' }}
+            />
+
+            <TrophyTwoTone
+                style={{ color: 'red', backgroundColor: 'red', fontSize: '50px', margin:'30px' }}
+            />
+
+            <PlayCircleFilled
+                style={{ color: 'green', backgroundColor: 'gray', fontSize: '50px', margin:'30px' }}
+            />
+
         </div>
     );
 };
--- pages/typography/index.js
+++ pages/typography/index.js
@@ -8,11 +8,11 @@ const index = () => {
             <Title level={1} className="text-center">Typography</Title>
 
             <Divider orientation="center" plain>I am a Divider</Divider>
-            <Title>I am h1</Title>
-            <Title level={2}>I am h2</Title>
-            <Title level={3}>I am h3</Title>
-            <Title level={4}>I am h4</Title>
-            <Title level={5}>I am h5</Title>
+            <Title style={{ color: 'blue' }}>I am h1</Title>
+            <Title style={{ color: 'red' }} level={2}>I am h2</Title>
+            <Title style={{ color: 'purple' }} level={3}>I am h3</Title>
+            <Title style={{ color: 'green' }} level={4}>I am h4</Title>
+            <Title style={{ color: 'goldenrod' }} level={5}>I am h5</Title>
         </div>
     );
 };
