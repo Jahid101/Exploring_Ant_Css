1a3f6933ff61901d2535a5b47e5b08fe619b11af
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-18 00:40:57 +0600

    upto form

---
+++ pages/inputNumber/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const index = () => {
+
+    return (
+        <div className="icons-list mv-50 mh-50">
+            <h1 className="text-center underline">Hey! Check out these input numbers</h1>
+
+        </div>
+    );
+};
+
+export default index;
